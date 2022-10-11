bce6bb4b7113fb66c3561dac5822b357d6968c12
ephraimibatt <ephraim@sterling.ng>
2022-10-11 10:40:05 +0100

    fixes new

--- lib/breakfast.dart
+++ lib/breakfast.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:recipe_with_ingredient/contants.dart';
 
 class BreakfastCategories extends StatelessWidget {
   const BreakfastCategories({super.key});
@@ -8,21 +9,26 @@ class BreakfastCategories extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Snacks'),
+        backgroundColor: Colors.transparent,
+      ),
       body: SafeArea(
-        child: Column(children: [
-          Container(),
-          Text('Snacks'),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+          Text('Snacks', style: TextStyle(color: primaryColor, fontSize: 30, fontWeight: FontWeight.w500),),
           Container(
             child: Row(
               children: [
                 Container(
                   child: Column(
                     children: [
-                      
-                           Container(
-                            child: Image(image: AssetImage('images/Rectangle1.png'),),
-                           ),
-                      
+                      Container(
+                        child: Image(
+                          image: AssetImage('images/Rectangle1.png'),
+                        ),
+                      ),
                     ],
                   ),
                 ),
@@ -34,4 +40,3 @@ class BreakfastCategories extends StatelessWidget {
     );
   }
 }
-
