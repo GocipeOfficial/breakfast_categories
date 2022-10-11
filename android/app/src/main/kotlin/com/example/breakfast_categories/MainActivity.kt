2ca525f628e9165b961ec9799939be030f2b5d30
Oyindobra <oyindobraagberebi.com>
2022-10-11 10:07:11 +0100

    initial commit

---
+++ android/app/src/main/kotlin/com/example/breakfast_categories/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.breakfast_categories
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/breakfast.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class BreakfastCategories extends StatelessWidget {
+  const BreakfastCategories({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(children: [
+          Container(),
+          Text('Snacks'),
+          Container(
+            child: Row(
+              children: [
+                Container(
+                  child: Column(
+                    children: [
+                      
+                           Container(
+                            child: Image(image: AssetImage('images/Rectangle`.png'),),
+                           ),
+                      
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
+
